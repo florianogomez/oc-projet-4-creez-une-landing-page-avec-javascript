e162d608b6acfa9d6a78b6b9e059192688d4c42a
Floriano GOMEZ <florianog96@gmail.com>
2024-11-08 11:23:11 +0100

    Feat : modal form validation
    - The form must be valid when the user clicks on “Submit”
    - First name field has a minimum of 2 characters / is not empty.
    - Last name field has a minimum of 2 characters / is not empty.
    - E-mail address is valid.
    - For the number of contests, a numerical value is entered.
    - A radio button is selected.
    - The general conditions box is checked, the other box is optional / can be left unchecked.
    - Keep the form data (do not delete the form) when it does not pass validation.

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -12,11 +12,11 @@ const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
 const modalCloseBtn = document.querySelectorAll(".close");
+const termsOfUseCheckbox = document.getElementById("checkbox1");
+const termsOfUseCheckboxError = document.getElementById("checkbox1-error");
 
-// launch modal event
-modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
 
-modalCloseBtn.forEach((btn) => btn.addEventListener("click", closeModal));
+document.addEventListener("DOMContentLoaded", onDocumentReady);
 
 // launch modal form
 function launchModal() {
@@ -27,4 +27,32 @@ function closeModal() {
   modalbg.style.display = "none";
 }
 
+function validate() {
+  console.log("checkbox1", termsOfUseCheckbox.checked);
+
+  if(!termsOfUseCheckbox.checked) {
+    showTermsOfUseError();
+    return false;
+  }
+
+  return true
+}
+
+function showTermsOfUseError() {
+  termsOfUseCheckbox.classList.add("field-error");
+  termsOfUseCheckboxError.style.display = "block";
+}
+
+function hideTermsOfUseError() {
+  termsOfUseCheckbox.classList.remove("field-error");
+  termsOfUseCheckboxError.style.display = "none";
+}
+
+function onDocumentReady() {
+	console.log("Document is ready");
+	modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
+	modalCloseBtn.forEach((btn) => btn.addEventListener("click", closeModal));
+  termsOfUseCheckbox.addEventListener("change", hideTermsOfUseError);
+  hideTermsOfUseError();
+}
 
